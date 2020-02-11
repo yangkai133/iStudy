51dff892ae0f81398ada30ce3298c7c42ee2adc2
Tony <tony@gmail.com>
2020-02-11 21:07:29 +0800

    First commit.

---
+++ app/src/main/java/com/example/chaxunzhanzuo/chaxunzhanzuo.java
@@ -0,0 +1,17 @@
+package com.example.chaxunzhanzuo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 10423 on 2020/2/4.
+ */
+
+public class chaxunzhanzuo extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.chaxunzhanzuo);
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/chaxunzhanzuo/zhujiemian.java
@@ -0,0 +1,143 @@
+package com.example.chaxunzhanzuo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * Created by HANGCHENG on 2020/2/4.
+ */
+
+public class zhujiemian extends AppCompatActivity {
+
+    private Button saoma;
+    private Button zxjl;
+    private Button cxzz;
+    private DrawerLayout mDrawerLayout;
+    private Button xj;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.zhujiemian);
+        super.onCreate(savedInstanceState);
+
+
+        /* 界面跳转设置**/
+        cxzz = (Button) findViewById(R.id.zj_cxzz);
+        cxzz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.setClass(zhujiemian.this, chaxunzhanzuo.class);
+                startActivity(intent);
+            }
+        });
+/*相机测试
+        xj = (Button) findViewById(R.id.xiangji);
+        xj.setOnClickListener(new View.OnClickListener() {
+                                  @Override
+                                  public void onClick(View v) {
+                                      Toast ts = Toast.makeText(getBaseContext(),"正在光速前往相机",Toast.LENGTH_LONG);
+
+                                      ts.show();
+
+        }
+                              });
+**/
+
+      /* toolbar 和侧滑界面设置**/
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.caidan);
+        }
+        navView.setCheckedItem(R.id.me_zh);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+
+
+            default:
+        }
+        return true;
+    }
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("zhujiemian Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
